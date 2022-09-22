e85016e2fcdd3f774cbf1790b8b3e1a2a46d7a68
ManhLe <manhle@email.com>
2022-09-22 11:46:53 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 
-  module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/tim-hoa-don/' // Thay tên repository của các bạn vào đây nhé
-      : '/'
-  }
-  
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/tim-hoa-don/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
+}
+//git init
+//git add -A
+//git commit -m "deploy"
+//git push -f https://github.com/manhle93/tim-hoa-don.git master:gh-pages
